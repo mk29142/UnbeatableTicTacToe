bbc374a5e5a9fb5fcff516add4fde9810d234f22
Mridul kumar <mridulkumar@Mriduls-MacBook-Pro.local>
2016-12-28 14:28:21 +0000

    Added game modes and minor bug fixes.

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -10,31 +10,35 @@ public class GameEngine {
 
     public GameEngine() {
         grid = new Grid();
-        ComputerMinMax computer = new ComputerMinMax(grid);
-        computer.setSeed(Content.CROSS);
-        int firstPlayer = -1;
+        ComputerMinMax computer1 = new ComputerMinMax(grid);
+        computer1.setSeed(Content.CROSS);
 
-        System.out.println("The symbol for computer is X and your symbol is O");
-        System.out.println("Press 0(zero) if you want to play first and 1(one) if you " +
-                "\nwant computer to play first");
+        ComputerMinMax computer2 = new ComputerMinMax(grid);
+        computer2.setSeed(Content.NOUGHT);
 
-        firstPlayer = in.nextInt();
+        int gameMode = -1;
 
-        while (firstPlayer < 0 || firstPlayer > 1) {
-            System.out.println("Please enter either 1 or 0");
+        System.out.println("Please choose gameMode");
+        System.out.println("Press 1 for Human Vs Computer, Press 2 for Human Vs Human" +
+                "\nand Press 3 for Computer Vs Computer.");
+
+        gameMode = in.nextInt();
+
+        while (gameMode < 1 || gameMode > 3) {
+            System.out.println("Please enter either 1, 2, or 3");
             while (!in.hasNextInt()) {
                 System.out.println("That's not a number!");
                 in.next();
             }
         }
 
+        initEngine(gameMode);
+
         System.out.println("Game grid before start");
         grid.drawGrid();
 
-        initEngine(firstPlayer);
-
         do {
-            playerMove(currPlayer, computer);
+            playerMove(currPlayer, computer1, computer2, gameMode);
             grid.drawGrid();
             updateGameState(currPlayer);
 
@@ -51,10 +55,7 @@ public class GameEngine {
                 System.out.println("=========================");
             }
 
-            if(currPlayer.equals(Content.CROSS))
-                currPlayer = Content.NOUGHT;
-            else if(currPlayer.equals(Content.NOUGHT))
-                currPlayer = Content.CROSS;
+            currPlayer = (currPlayer.equals(Content.CROSS)) ? Content.NOUGHT : Content.CROSS;
         }
 
         // keep on playing until one player wins or the game is drawn
@@ -72,49 +73,141 @@ public class GameEngine {
         }
     }
 
-    private void playerMove(Content currPlayer, ComputerMinMax computer) {
-        boolean validInput = false;
+    private void playerMove(Content currPlayer, ComputerMinMax computer1, ComputerMinMax computer2, int gameMode) {
 
         int row = -1;
         int col = -1;
 
-        //computer's go
-        if(currPlayer.equals(Content.CROSS)) {
+        while (true) {
 
-            int [] computerMoves = computer.move();
-            row = computerMoves[0];
-            col = computerMoves[1];
-            System.out.println("");
-            System.out.println("Computer placed its " + currPlayer + " at "+ row + " " + col);
+            if(gameMode == 1) {
 
-        } else if(currPlayer.equals(Content.NOUGHT)) {
-            //Humans go
+                //computer's go
+                if(currPlayer.equals(Content.CROSS)) {
 
-            System.out.println("");
-            System.out.println("Player O please enter the location where you want to place your " + currPlayer + "\n"
-                    + "The input should be (row[0-2] , column[0-2]) WITHOUT commas, and ONLY SPACES between two digits");
+                    int [] computerMoves = computer1.move();
+                    row = computerMoves[0];
+                    col = computerMoves[1];
+                    System.out.println("");
+                    System.out.println("Computer placed its " + currPlayer + " at "+ row + " " + col);
 
-            row = in.nextInt();
-            col = in.nextInt();
+                } else if(currPlayer.equals(Content.NOUGHT)) {
+                    //Humans go
 
-            System.out.println("");
+                    System.out.println("");
+                    System.out.println("Player O please enter the location where you want to place your " + currPlayer + "\n"
+                            + "The input should be (row[0-2] , column[0-2]) WITHOUT commas, and ONLY SPACES between two digits");
+
+                    row = in.nextInt();
+                    col = in.nextInt();
+
+                    System.out.println("");
+                }
+
+            } else if(gameMode == 2) {
+
+                System.out.println("");
+                System.out.println("Player O please enter the location where you want to place your " + currPlayer + "\n"
+                        + "The input should be (row[0-2] , column[0-2]) WITHOUT commas, and ONLY SPACES between two digits");
+
+                row = in.nextInt();
+                col = in.nextInt();
+
+                System.out.println("");
+
+            } else if(gameMode == 3) {
+
+                int [] computerMoves;
+
+                if(currPlayer.equals(Content.CROSS)) {
+                    computerMoves = computer1.move();
+                } else {
+                    computerMoves = computer2.move();
+                }
+
+                row = computerMoves[0];
+                col = computerMoves[1];
+                System.out.println("");
+                System.out.println("Computer placed its " + currPlayer + " at "+ row + " " + col);
+
+            }
+
+            if(row >= 0 && row < 3 && col >= 0 && col < 3 && grid.getContent(row, col).equals(Content.EMPTY)) {
+                grid.setCell(row, col, currPlayer);
+                break;
+            }
+            else {
+                System.out.println("");
+                System.out.println("This move at (" + row + "," + col
+                        + ") is not valid. Try again...");
+            }
         }
+    }
 
-        if(row >= 0 && row < 3 && col >= 0 && col < 3 && grid.getContent(row, col).equals(Content.EMPTY)) {
-            grid.setCell(row, col, currPlayer);
-        }
-        else {
-            System.out.println("");
-            System.out.println("This move at (" + (row + 1) + "," + (col + 1)
-                    + ") is not valid. Try again...");
+    private void initEngine(int gameMode) {
+        grid.clearGrid();
+        currState = State.PLAYING;
+
+        switch (gameMode) {
+            case 1:
+                humanVComputer();
+                break;
+            case 2:
+                humanVHuman();
+                break;
+            case 3:
+                computerVComputer();
+                break;
         }
 
     }
 
-    public void initEngine(int first) {
-        grid.clearGrid();
-        currState = State.PLAYING;
-        currPlayer = (first == 0) ? Content.NOUGHT : Content.CROSS;
+    private void computerVComputer() {
+
+        currPlayer = Content.CROSS;
+    }
+
+    private void humanVHuman() {
+
+        int firstPlayer = -1;
+
+        System.out.println("The symbol for player 2 is X and your symbol is O");
+        System.out.println("Press 0(zero) if you want to play first and 1(one) if you " +
+                "\nwant computer to play first");
+
+        firstPlayer = in.nextInt();
+
+        while (firstPlayer < 0 || firstPlayer > 1) {
+            System.out.println("Please enter either 1 or 0");
+            while (!in.hasNextInt()) {
+                System.out.println("That's not a number!");
+                in.next();
+            }
+        }
+
+        currPlayer = (firstPlayer == 0) ? Content.NOUGHT : Content.CROSS;
+    }
+
+    private void humanVComputer() {
+
+        int firstPlayer = -1;
+
+        System.out.println("The symbol for computer is X and your symbol is O");
+        System.out.println("Press 0(zero) if you want to play first and 1(one) if you " +
+                "\nwant computer to play first");
+
+        firstPlayer = in.nextInt();
+
+        while (firstPlayer < 0 || firstPlayer > 1) {
+            System.out.println("Please enter either 1 or 0");
+            while (!in.hasNextInt()) {
+                System.out.println("That's not a number!");
+                in.next();
+            }
+        }
+
+        currPlayer = (firstPlayer == 0) ? Content.NOUGHT : Content.CROSS;
+
     }
 
     public State getState() {
