8a69b9efe363d2492cd8e400b460c3b38f92b298
Mridul kumar <mridulkumar@Mriduls-MacBook-Pro.local>
2016-12-28 14:34:23 +0000

    Minor style fixes.

--- src/Main.java
+++ src/Main.java
@@ -11,46 +11,38 @@ public class Main {
             System.out.println("--------------------");
     }
 
-    static void showInstructions()
-    {
+    static void showInstructions() {
         System.out.println(" Instructions:"
                 + "\n ======================"
-                + "\n This is a Tic-Tac-Toe game. Your will be the first turn to play and you symbol is a"
-                + "\n NOUGHT (o) and computer's symbol is a CROSS (X). You have to beat the computer."
-                + "\n You have to enter the row and column number of the position you want to place"
-                + "\n your symbol on the grid, with space between the two numbers. Please DON'T"
-                + "\n separate the numbers with a comma."
-                + "\n All the best. You will need luck to beat the computer player \n");
+                + "\n This is a Tic-Tac-Toe game. There are 3 game modes, Human vs Human, " +
+                "Human vs Computer and Computer vs Computer." +
+                "When prompted for an input please enter the row and column number in that " +
+                "order WITHOUT commas and ONLY a space.\n");
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         State continueGame = null;
 
         int choice = 0;
-        do{
+        do {
             showMenu();
-            try
-            {
+            try {
                 choice = input.nextInt();
             }
-            catch(Exception e)
-            {
+            catch(Exception e) {
                 System.out.println("invalid input");
             }
-            while (choice <= 0 || choice > 4)
-            {
+            while (choice <= 0 || choice > 4) {
                 System.out.println("Please enter number between 1 to 4!");
-                while (!input.hasNextInt())
-                {
+                while (!input.hasNextInt()) {
                     System.out.println("That's not a number!");
-                    input.next(); // this is important!
+                    input.next();
                 }
                 choice = input.nextInt();
             }
 
-            switch(choice)
-            {
+            switch(choice) {
 
                 case 1:
                     showInstructions();
@@ -69,7 +61,7 @@ public class Main {
                     choice = 0;
             }
         }
-        while(choice >= 0 && choice<4 || continueGame != null);
+        while(choice >= 0 && choice < 4 || continueGame != null);
 
     }
 }
