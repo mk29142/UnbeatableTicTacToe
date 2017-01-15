e93dece4a65c3fad29571d4f69f0e5610769a098
Mridul Kumar <mk1514@point56.doc.ic.ac.uk>
2017-01-15 13:45:54 +0000

    removing director

--- src/tictactoe/data/Content.java
+++
@@ -1,5 +0,0 @@
-package tictactoe.data;
-
-public enum Content {
-    NOUGHT, CROSS, EMPTY;
-}
--- src/tictactoe/data/State.java
+++
@@ -1,6 +0,0 @@
-package tictactoe.data;
-
-public enum State {
-
-    PLAYING, DRAW, NOUGHT_WON, CROSS_WON;
-}
